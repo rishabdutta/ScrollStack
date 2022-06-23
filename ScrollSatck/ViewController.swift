3a6011e92ba7b11d7582ef7c5f86b4e762e5578f
Rishab Dutta <rishab.dutta@inmobi.com>
2022-06-23 19:38:22 +0530

    Initial Commit

---
+++ ScrollSatck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScrollSatck
+//
+//  Created by rishab.dutta on 23/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
